7d9dc269a62d0e48429479b32bfafd658586d9bb
Alain Marchand <amarchan@matrox.com>
2020-04-01 14:35:41 -0400

    Files from the schematic review of the sensor board

---
+++ athena/doc/pinout/xilinx_schematic_review/from_cad/pin_change_altera.tcl
@@ -0,0 +1,21 @@
+# Generated 2020/03/30 07:46
+# V7->W2
+set_location_assignment PIN_W2 -to xgs_hispi_sdata_n[5]
+# U7->V2
+set_location_assignment PIN_V2 -to xgs_hispi_sdata_p[5]
+# V5->V7
+set_location_assignment PIN_V7 -to xgs_hispi_sdata_n[4]
+# U5->U7
+set_location_assignment PIN_U7 -to xgs_hispi_sdata_p[4]
+# W3->T2
+set_location_assignment PIN_T2 -to xgs_hispi_sdata_n[3]
+# V3->R2
+set_location_assignment PIN_R2 -to xgs_hispi_sdata_p[3]
+# W2->V5
+set_location_assignment PIN_V5 -to xgs_hispi_sdata_n[2]
+# V2->U5
+set_location_assignment PIN_U5 -to xgs_hispi_sdata_p[2]
+# T2->W3
+set_location_assignment PIN_W3 -to xgs_hispi_sdata_n[0]
+# R2->V3
+set_location_assignment PIN_V3 -to xgs_hispi_sdata_p[0]
